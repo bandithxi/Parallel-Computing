706070cd6b29ebdaac3e43ad78a9082d86d3bea3
bandithxi <bandithxi@hotmail.com>
2014-11-07 01:17:26 -0800

    Moved repository

--- DesignPatternExamples/Singleton/OompaloompaBar.java
+++
@@ -1,7 +0,0 @@
-
-public class OompaloompaBar extends ChocolateBar{
-	public OompaloompaBar(int id){
-		super(id);
-		this.name = "OompaloompaBar";
-	}
-}
--- DesignPatternExamples/Singleton/WonkaBar.java
+++
@@ -1,7 +0,0 @@
-
-public class WonkaBar extends ChocolateBar {
-	public WonkaBar(int id) {
-		super(id);
-		name = "WonkaBar";
-	}
-}
